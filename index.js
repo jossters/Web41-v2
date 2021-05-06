be05385f89ea5139bc11e6506d13a80512e0dcc8
Johnny Murillo <jossters@gmail.com>
2021-05-06 15:33:16 -0700

    wip

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
-
+const path = require("path")
 const express = require('express')
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
+
+process.env.API_KEY || "gaga"
+
+app.use(express.static(path.join(__dirname, "client/build")))
 
 app.use(express.json())
 
@@ -13,6 +17,10 @@ app.get('/api/*', (req, res) => {
         cohort: "Web41",
         Working: "Yes"
     })
+})
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 app.listen(PORT, () => {
